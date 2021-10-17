7274018b01dacdae129fffc35d1753b35d162d79
sh1eldma1den <mor02012@byui.edu>
2021-10-17 00:53:57 -0400

    preston again

---
+++ lesson5/js/pancakes.js
@@ -0,0 +1,7 @@
+function pancakes() {
+    var d = new Date();
+    var n = d.getDay();
+    if (n == 5) {
+    document.getElementById('pancakes-span').innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+  }
+}
