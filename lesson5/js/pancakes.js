ec32d1e369a09c4d4be1ee4cb19fa21166f55ec3
sh1eldma1den <mor02012@byui.edu>
2021-10-19 13:00:31 -0400

    fixed js

--- lesson5/js/pancakes.js
+++ lesson5/js/pancakes.js
@@ -1,7 +1,4 @@
-function pancakes() {
-    var d = new Date();
-    var n = d.getDay();
+ var d = new Date();
+var n = d.getDay();
     if (n == 5) {
-    document.getElementById('pancakes-span').innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-  }
-}
+    document.getElementById('pancakes-span').innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";}
