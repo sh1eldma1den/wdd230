d9127a3f2be00b3529990c87c25c4d7b7a0a4cb9
sh1eldma1den <mor02012@byui.edu>
2021-11-03 17:34:10 -0400

    .

---
+++ lesson8/lesson7/gallery/js/pancakes.js
@@ -0,0 +1,4 @@
+var d = new Date();
+var n = d.getDay();
+    if (n == 5) {
+    document.getElementById('pancakes-span').innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";}
