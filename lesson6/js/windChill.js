e4f26da15f1638f6456f7a5624fda4c73090d776
sh1eldma1den <mor02012@byui.edu>
2021-11-21 11:21:51 -0500

    .

--- lesson6/js/windChill.js
+++ lesson6/js/windChill.js
@@ -5,7 +5,7 @@ if ( x >= 50 && y >=3 ) {
         let z = Math.pow(y,0.16); 
         let result = 35.74 + (0.6215 * x) - (35.75 * z) + (0.4275 * x * z);
         result = Math.round(output*100)/100;
-        document.querySelector('#windChill').textcontent = 'Wind Chill: ' + result;
+        document.querySelector('#windChill').textcontent = result;
     }
 
 
