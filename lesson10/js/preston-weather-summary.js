e582cf00f02bd8655e274e549af3c75c36895578
sh1eldma1den <mor02012@byui.edu>
2021-11-21 10:58:51 -0500

    .

--- lesson10/js/preston-weather-summary.js
+++ lesson10/js/preston-weather-summary.js
@@ -2,14 +2,12 @@ const apiURL ="http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    document.querySelector("#currently").textContent =
-      jsObject.weather[0].description.toUpperCase();
-
     const temperature = jsObject.main.temp.toFixed(1);
-    document.querySelector("#temperature").textContent = temperature;
-    document.querySelector("#humidity").textContent =
-      jsObject.main.humidity.toFixed(1);
     const windspeed = jsObject.wind.speed.toFixed(1);
+    
+    document.querySelector("#currently").textContent = jsObject.weather[0].description.toUpperCase();
+    document.querySelector("#temperature").textContent = temperature;
+    document.querySelector("#humidity").textContent = jsObject.main.humidity.toFixed(1);
     document.querySelector("#windSpeed").textContent = windspeed;
 
     //Calculates the wind chill. Returns "N/A" if wind speed is < 3mph and/or temp is >50 
@@ -22,7 +20,6 @@ fetch(apiURL)
       document.querySelector("#windChill").textContent = windchill.toFixed(1);
     } else {
       const windChillNA = "N/A";
-      document.querySelector("#windChill").textContent =
-        windChillNA;
+      document.querySelector("#windChill").textContent = windChillNA;
     }
   });
