5d14a18a50317c0a564cb9f982170274188b59a0
sh1eldma1den <mor02012@byui.edu>
2021-11-24 19:16:35 -0500

    .

---
+++ lesson11/js/sodasprings-forecast.js
@@ -0,0 +1,51 @@
+const today = new Date();
+const options = { weekday: "short" };
+let forecastDay0 = new Intl.DateTimeFormat("en-US", options).format(
+  today.getTime() + 86400000
+);
+let forecastDay1 = new Intl.DateTimeFormat("en-US", options).format(
+  today.getTime() + 86400000 * 2
+);
+let forecastDay2 = new Intl.DateTimeFormat("en-US", options).format(
+  today.getTime() + 86400000 * 3
+);
+let forecastDay3 = new Intl.DateTimeFormat("en-US", options).format(
+  today.getTime() + 86400000 * 4
+);
+let forecastDay4 = new Intl.DateTimeFormat("en-US", options).format(
+  today.getTime() + 86400000 * 5
+);
+
+let forecastDay = [
+  forecastDay0,
+  forecastDay1,
+  forecastDay2,
+  forecastDay3,
+  forecastDay4,
+];
+
+for (let i = 0; i < 5; i++) {
+  document.getElementById(`forecast-day${[i]}`).textContent = forecastDay[i];
+}
+
+const apiForecastURL ="https://api.openweathermap.org/data/2.5/forecast?id=5607916&appid=b599c9492b477e55d6b80923d6abd3ea&units=imperial";
+fetch(apiForecastURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    const eveForecast = jsObject.list.filter((x) =>
+      x.dt_txt.includes("18:00:00")
+    );
+    for (let j = 0; j < 5; j++) {
+      document.querySelector(`#forecast-temp${[j]}`).innerHTML = `${eveForecast[
+        j
+      ].main.temp.toFixed(1)} &#8457`;
+      const imagesrc = `http://openweathermap.org/img/w/${eveForecast[j].weather[0].icon}.png`;
+      const forecastDescription = eveForecast[j].weather[0].description;
+      document
+        .querySelector(`#forecast-icon${[j]}`)
+        .setAttribute("src", imagesrc);
+      document
+        .querySelector(`#forecast-icon${[j]}`)
+        .setAttribute("alt", forecastDescription);
+    }
+  });
