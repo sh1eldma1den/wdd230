110f2b00a0ea4a97df488aeba586ef4647a872ef
sh1eldma1den <mor02012@byui.edu>
2021-11-24 19:25:00 -0500

    .

---
+++ lesson11/js/fishhaven-weather-summary.js
@@ -0,0 +1,33 @@
+
+const apiURL ="//api.openweathermap.org/data/2.5/forecast?lat=42.051601&lon=-111.253532&appid=b599c9492b477e55d6b80923d6abd3ea&units=imperial";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    
+    console.log(jsObject.list[0].main.temp);
+
+    const temperature = Math.round(jsObject.list[0].main.temp);
+    document.querySelector("#temperature").textContent = temperature;
+    
+    document.querySelector("#currently").textContent = jsObject.list[0].weather[0].description.toUpperCase();  
+  
+    const windspeed = Math.round(jsObject.list[0].wind.speed);
+    document.querySelector("#windSpeed").textContent = windspeed + " MPH";
+ 
+
+
+    document.querySelector("#humidity").textContent = Math.round(jsObject.list[0].main.humidity);
+    
+    //Calculates the wind chill. Returns "N/A" if wind speed is < 3mph and/or temp is >50 
+    if (temperature <= 50 && windspeed > 3) {
+      const windchill =
+        35.74 +
+        0.6215 * temperature -
+        35.75 * Math.pow(windspeed, 0.16) +
+        0.4275 * temperature * Math.pow(windspeed, 0.16);
+      document.querySelector("#windChill").textContent = windchill.toFixed(1);
+    } else {
+      const windChillNA = "N/A";
+      document.querySelector("#windChill").textContent = windChillNA;
+    }
+  });
