b0fefc5754e35a0b754fd871eae4768bad2dc843
sh1eldma1den <mor02012@byui.edu>
2021-11-29 10:58:33 -0500

    .

---
+++ lesson11/js/fishhaven-events.js
@@ -0,0 +1,13 @@
+const requestURL = "//byui-cit230.github.io/weather/data/towndata.json";
+fetch(requestURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    const towns = jsObject["towns"]; 
+    const townsFiltered = towns.filter((x) => x.name.includes("Fish Haven"));
+    let length = townsFiltered[0].events.length;
+    for (let i = 0; i < length; i++) {
+      let townEvent = document.createElement("li");
+      townEvent.textContent = townsFiltered[0].events[i];
+      document.querySelector("ul.events").appendChild(townEvent);
+    }
+  });
