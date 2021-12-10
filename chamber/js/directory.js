2332ee1cc0b435421931845124cb86e0d0e2d9b1
sh1eldma1den <mor02012@byui.edu>
2021-12-09 21:44:06 -0500

    .

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,3 +1,10 @@
-$.getJSON("directory.json", function(json) {
-    console.log(json);
-});
+const requestURL = "//byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing;
+
+  })
